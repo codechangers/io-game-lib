e20055a1faced9f4b6ff05df48f0ad376d296e1a
Macuyler <macuyler@gmail.com>
2020-10-19 16:11:35 -0500

    Add submodule sync script

---
+++ bin/pull_subs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+hq=$(pwd)
+
+grep 'path' .gitmodules | cut -d'=' -f2 | awk '{$1=$1};1' | while read -r module ; do
+	echo "Processing $module"
+	cd $module
+	git pull
+	cd $hq
+	updated=$(git status | grep "$module (new commits)" | wc -l)
+	if [ "$updated" -eq "1" ]; then
+		echo "$module was updated."
+		git add $module
+		git commit -m "Pull $module updates"
+	else
+		echo "$module is the same."
+	fi
+done
+
+
+
