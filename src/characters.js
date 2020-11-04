0a6533a96d904629c0f12e2a4f7807d0a4b57c13
Tourtle21 <kardlargin@gmail.com>
2020-11-03 18:48:24 -0700

    Added changes for attaching things

--- src/characters.js
+++ src/characters.js
@@ -27,11 +27,14 @@ function getCharacters(
     game.room.listen(`${type}/:id`, function (change) {
       if (change.operation == 'add') {
         const { id, x, y } = change.value;
-        const sprite = game.add.sprite(x, y, type);
+        let sprite = game.add.container(x, y);
+        let character = game.front_layer.create(0, 0, type);
+        sprite.add([character]);
         sprite.setScale(game.scales[type] || 1);
         game[type][id] = {
           sprite,
           ...change.value,
+          attached: {}
         };
         onAdd(game[type][id], change.value);
       } else if (change.operation == 'remove') {
@@ -42,9 +45,49 @@ function getCharacters(
       }
     });
     game.room.listen(`${type}/:id/:attribute`, function (change) {
+      if (change.operation == 'add' && change.value.type) {
+        let x = game[type][change.value.id].sprite.x;
+        let y = game[type][change.value.id].sprite.y;
+        if (change.value.type == 'text') {
+          let text = game.add.text(x + change.value.x, y + change.value.y, ` ${change.value.text} `, { color: 'white', backgroundColor: 'rgba(0,0,0,0.7)' }).setScale(change.value.scale)
+          game[type][change.value.id].attached[change.value.name] = {...change.value, sprite:text};;
+        }
+        if (change.value.type == 'item') {
+          let item = game.front_layer.create(x + change.value.x, y + change.value.y, change.value.image).setScale(change.value.scale);
+          game[type][change.value.id].sprite.add(item);
+          game[type][change.value.id].attached[change.value.name] = {...change.value, sprite:item};
+        }
+        if (change.value.type == 'bar') {
+          var rect = new Phaser.Geom.Rectangle(0, 0, change.value.width, change.value.height);
+          var graphics = game.add.graphics({ fillStyle: { color: `0x999999` } });
+          rect = graphics.fillRectShape(rect);
+          game[type][change.value.id].attached[`${change.value.name}Background`] = {...change.value, sprite:rect};
+          var newRect = new Phaser.Geom.Rectangle(0, 0, change.value.width, change.value.height);
+          var graphics = game.add.graphics({ fillStyle: { color: `0x999900` } });
+          newRect = graphics.fillRectShape(newRect);
+          rect.x = x + change.value.x;
+          newRect.x = x + change.value.x;
+          rect.y = y + change.value.y;
+          newRect.y = y + change.value.y;
+          newRect.setScale(100/change.value, 1);
+          game[type][change.value.id].attached[change.value.name] = {...change.value, sprite:newRect};
+        }
+      }
+      if (change.operation == 'remove') {
+        game[type][change.path.id].attached[change.path.attribute].sprite.destroy();
+        if (game[type][change.path.id].attached[change.path.attribute].type == 'bar') {
+          game[type][change.path.id].attached[`${change.path.attribute}Background`].sprite.destroy();
+        }
+        delete game[type][change.path.id].attached[change.path.attribute];
+      }
       if (change.operation == 'replace') {
         const { id, attribute } = change.path;
         if (attribute == 'x' || attribute == 'y') {
+          for (let item in game[type][id].attached) {
+            if (game[type][id].attached[item].type !== 'item') game[type][id].attached[item].sprite[attribute] = change.value + game[type][id].attached[item][attribute];
+          }
+          game[type][id].sprite[attribute] = change.value;
+        } else if (attribute == 'angle') {
           game[type][id].sprite[attribute] = change.value;
         } else {
           game[type][id][attribute] = change.value;
@@ -55,6 +98,13 @@ function getCharacters(
   } else {
     this.addConnectEvent('getCharacters', [type, onAdd, onRemove, onUpdate]);
   }
+  game.room.listen(`${type}/:id/:attribute/:id`, function (change) {
+    console.log(change);
+    if (change.path.id === 'filled') {
+      console.log(game[type][change.rawPath[1]].attached[change.rawPath[2]].sprite)
+      game[type][change.rawPath[1]].attached[change.rawPath[2]].sprite.setScale(change.value/100, 1);
+    }
+  });
 }
 
 const client = { addCharacters, getCharacters };
@@ -109,12 +159,41 @@ function nextCharacterId(
   return `${type}${Object.keys(this.game.state[type]).length + 1}`;
 }
 
+// Attach something to the character or other things.
+function attachTo(
+  type, // string: The type of character/resource
+  id, // string: A unique character id that you want to attach it to
+  data /* object: {
+    name | string: name of the thing you want to attach
+    x | int: x position relative to character
+    y | int: y position relative to character
+    scale | int : number between 0 and 1 to represent size
+    type | string : either 'item', 'bar', or 'text'
+    image | string : name of image if it is an ITEM 
+    text | string : text if it is a TEXT
+    filled | int : amount of bar filled if it is a BAR
+  }
+  */
+) {
+  this.game.state[type][id][data.name] = {...data, id};
+}
+
+function unAttach(
+  type, // string: the type of character/resource
+  id, // string: A unique character id that you want to attach it to
+  name // string: name of the item you want to unattach
+) {
+  delete this.game.state[type][id][name];
+}
+
 const server = {
   setupCharacters,
   createACharacter,
   getACharacter,
   deleteACharacter,
   nextCharacterId,
+  attachTo,
+  unAttach
 };
 
 module.exports = { client, server };
